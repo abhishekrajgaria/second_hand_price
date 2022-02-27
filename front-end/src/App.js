45dd6d0140bdf6fb726db2ba9bf5bb91e78d0888
abhrajga <abhishek.rajgaria@publicissapient.com>
2022-02-27 20:15:01 +0530

    add bootstrap using CDN

--- front-end/src/App.js
+++ front-end/src/App.js
@@ -1,23 +1,10 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import Main from "./components/main/Main"
 
-function App() {
+function App(props) {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={"/logos/car_logo.png"} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container">
+      <Main/>
     </div>
   );
 }
---
+++ front-end/src/components/main/Main.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function Main(props) {
+    return (
+        <div className="container">
+            <h1>Start of the Project: Good Car Deals</h1>
+        </div>
+    );
+}
+
+export default Main;
